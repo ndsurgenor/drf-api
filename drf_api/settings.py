6e4c4b69eb4ec7d7c2e0bd4a00bfd3de0d3c8460
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-06 09:34:11 +0000

    Add profiles app and model view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -46,9 +46,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'profiles',
 ]
 
 MIDDLEWARE = [
