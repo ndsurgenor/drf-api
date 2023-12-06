c205bb0b1221f1ff549f3ec0185eace65ffb2052
Nathan Surgenor <ndsurgenor@gmail.com>
2023-12-06 12:04:00 +0000

    Add django rest framework and initial imports to views.py

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_profile_n85cdj')
+    image = models.ImageField(upload_to='images/', default='../default_profile_n85cdj.jpg')
 
     class Meta:
         ordering = ['-created_at']
